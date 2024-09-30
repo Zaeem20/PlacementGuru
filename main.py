1b6ba7f0ac335f2c9b9cb9b0ed32647f40d51d8e
Zaeem20 <duranizaeem@gmail.com>
2024-09-30 12:25:33 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from click import option
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import google.generativeai as genai
@@ -5,6 +6,15 @@ import google.generativeai as genai
 st.set_page_config(page_title='PlacementGuru', layout='wide')
 
 
+st.title('Placement Guru')
+col1, col2 = st.columns(2)
+with col1:
+    sec1,sec2 = st.columns(2)
+    with sec1:
+        st.text_input('Role', placeholder='What role your are seeking for!')
+        st.selectbox('Company', options=('', 'Google', 'Meta', 'Wipro', 'Accenture'))
+    with sec2:
+        st.selectbox('Interviewer', options = ('Professional', 'Technical', 'Behaviour'))
 
-st.write('Hello world')
-webrtc_streamer('record interview') 
+with col2:
+    webrtc_streamer('record interview') 
