a2a81d923bf62e75bea058663e9a4a1d681701bc
ark331 <arkhams312@gmail.com>
2024-09-30 16:58:02 +0530

    Update

--- pages/contact.py
+++ pages/contact.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title='PlacementGuru', layout='wide')
 with st.form("contact_form"):
     st.text_input("Name",max_chars=100)
     st.text_input("Email")
-    st.number_input("Phone Number",max_value=13)
+    st.number_input("Phone Number")
     st.text_area("Message")
     st.form_submit_button("Submit")
 
